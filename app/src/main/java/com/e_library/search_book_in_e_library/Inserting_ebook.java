7b6749b2967999cefd848ab4440e71ffde2d205e
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-06 19:00:36 +0200

    Creating Bookstore activity and designing it

--- app/src/main/java/com/e_library/search_book_in_e_library/Inserting_ebook.java
+++ app/src/main/java/com/e_library/search_book_in_e_library/Inserting_ebook.java
@@ -1,6 +1,5 @@
 package com.e_library.search_book_in_e_library;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Bookstore extends AppCompatActivity {
+public class Inserting_ebook extends AppCompatActivity {
 
-    DatabaseHelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookstore);
-        myDB = new DatabaseHelper(this);
-
+        setContentView(R.layout.activity_inserting_ebook);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,10 +24,6 @@ public class Bookstore extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-    }
-    public void Insert_ebook(View view){
-        Intent intent = new Intent(Bookstore.this, Inserting_ebook.class);
     }
 
 }
